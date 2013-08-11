30e3b05a371f8d53ed6cce26925b76736529d023
Micha Kops <micha.kops@gmail.com>
2013-08-11 16:49:37 +0200

    Chat app layout modified.

--- src/main/java/com/hascode/tutorial/ChatEndpoint.java
+++ src/main/java/com/hascode/tutorial/ChatEndpoint.java
@@ -5,19 +5,23 @@ import java.util.Date;
 
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/chat")
+@ServerEndpoint("/chat/{userName}/{room}")
 public class ChatEndpoint {
 
 	@OnMessage
-	public void onMessage(final Session session, final String message) {
+	public void onMessage(final Session session, final String message,
+			@PathParam("userName") final String userName,
+			@PathParam("room") final String room) {
 		try {
 			for (Session s : session.getOpenSessions()) {
 				if (s.isOpen()) {
 					s.getBasicRemote().sendText(
-							"Message received at " + new Date().toString()
-									+ ": " + message);
+							String.format("(%s) %s@%s: %s",
+									new Date().toString(), userName, room,
+									message));
 				}
 			}
 		} catch (IOException e) {
