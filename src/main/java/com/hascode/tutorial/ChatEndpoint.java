3281e8525eef63202641dbaa2e5ebb8cb5f490c4
Micha Kops <micha.kops@gmail.com>
2013-08-12 21:11:07 +0200

    Message encoders+decoders implemented.

--- src/main/java/com/hascode/tutorial/ChatEndpoint.java
+++ src/main/java/com/hascode/tutorial/ChatEndpoint.java
@@ -2,30 +2,31 @@ package com.hascode.tutorial;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.websocket.EncodeException;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/chat/{userName}/{room}")
+@ServerEndpoint(value = "/chat/{room}", encoders = ChatMessageEncoder.class, decoders = ChatMessageDecoder.class)
 public class ChatEndpoint {
+	private final Logger log = Logger.getLogger(getClass().getName());
 
 	@OnMessage
-	public void onMessage(final Session session, final String message,
-			@PathParam("userName") final String userName,
+	public void onMessage(final Session session, final ChatMessage chatMessage,
 			@PathParam("room") final String room) {
 		try {
+			chatMessage.setReceived(new Date());
 			for (Session s : session.getOpenSessions()) {
 				if (s.isOpen()) {
-					s.getBasicRemote().sendText(
-							String.format("(%s) %s@%s: %s",
-									new Date().toString(), userName, room,
-									message));
+					s.getBasicRemote().sendObject(chatMessage);
 				}
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (IOException | EncodeException e) {
+			log.log(Level.WARNING, "onMessage failed", e);
 		}
 	}
 }
